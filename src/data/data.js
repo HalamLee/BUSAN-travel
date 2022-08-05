2c3bfca4c9a190e9b8aa88cd495dbf642ae8fba1
Lami <Halam.Lee.Lami@gmail.com>
2022-08-05 23:00:24 +0900

    :card_file_box: 데이터 파일 분리

---
+++ src/data/data.js
@@ -0,0 +1,46 @@
+// 08/08
+export const firstPositions = [
+  {
+    title: '브라운도트',
+    latlng: { lat: 35.1686008, lng: 129.1141368 },
+  },
+
+  {
+    title: '수변최고돼지국밥 본점',
+    latlng: { lat: 35.1569, lng: 129.1343 },
+  },
+  {
+    title: '광안리 요이쿠마',
+    latlng: { lat: 35.1581, lng: 129.1266 },
+  },
+  {
+    title: '류센소 광안직영점',
+    latlng: { lat: 35.1498, lng: 129.1147 },
+  },
+];
+
+// 08/09
+export const secondPositions = [
+  {
+    title: '코모도호텔',
+    latlng: { lat: 35.1092, lng: 129.0355 },
+  },
+  {
+    title: '뚱보집',
+    latlng: { lat: 35.1007, lng: 129.0361 },
+  },
+];
+
+// 08/10
+export const lastPositions = [
+  {
+    title: '부산종합버스터미널',
+    latlng: { lat: 35.285, lng: 129.0954 },
+  },
+  {
+    title: '신발원',
+    latlng: { lat: 35.1149, lng: 129.0387 },
+  },
+];
+
+export const all = [...firstPositions, ...secondPositions, ...lastPositions];
