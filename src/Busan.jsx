d09e5269bc8e86f36f1703e255353c4df5cff62d
Lami <Halam.Lee.Lami@gmail.com>
2022-08-08 00:00:32 +0900

    :rocket: 지도 크기 줄여서 배포

--- src/Busan.jsx
+++ src/Busan.jsx
@@ -62,7 +62,7 @@ function Busan() {
         style={{
           // 지도의 크기
           width: '350px',
-          height: '400px',
+          height: '350px',
         }}
         level={3} // 지도의 확대 레벨
       >
